2f73ee589128bfaa2b39bcb9700ad223dfc6b60b
bimogempar <bimogempar@gmail.com>
2021-12-12 15:03:00 +0700

    fix state active navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,13 +5,13 @@
     <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
         <ul class="navbar-nav mx-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
+                <a class="nav-link{{ request()->is('/') ? ' active' : ''}}" href="/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="vaccine">Vaccine</a>
+                <a class="nav-link{{ request()->is('vaccine') ? ' active' : ''}}" href="/vaccine">Vaccine</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="patient">Patient</a>
+                <a class="nav-link{{ request()->is('patient') ? ' active' : ''}}" href="/patient">Patient</a>
         </ul>
     </div>
 </nav>
